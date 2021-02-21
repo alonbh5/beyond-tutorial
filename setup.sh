481c07281dba6a352e81e82e03792827f0312990
Alon Ben Harosh <alonbh5@gmail.com>
2021-02-21 14:48:35 +0200

    Adding a Django devlopment environment
    - Setup [Pipenv][1] to install and mange Django and other Pyhton
    dependencies
    - Bootstrappen an empty [Django][2] application
    - Setup Vagrant to automatically bring out application up as well as
    make it accessible from a web browser
    
    After running `vagrant up` the appliction can be accessed at:
    
    >http://127.0.0.1:8000/
    
    **Note:** Most changes had ben auto-generated, only `Vagrantfile` and
    `setup.sh` had been edited manually.
    
    [1]: https://hithub.com/pypa/pipenv
    [2]: https://www.djangoproject.com/

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -
+# The -e option would make out script exit with an error if any command
+# fails while the -x option makes verbosely it output what it does
+
+# Install pipenv, the -n option makes sudo fail instead of asking for a
+# password if we don't have sufficient privilages to run it
+sudo -n dnf install -y pipenv
+
+cd /vagrant
+# Install dependencies with Pipenv
+pipenv sync --dev
+
+# run out app. nohup and "&" are used to let the setup script finish
+# while our app stays up.  The app logs will be collected in nohup.out
+nohup pipenv run python manage.py runserver 0.0.0.0:8000  
